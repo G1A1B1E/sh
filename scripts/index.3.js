31830aa0e8da20c5f70b0f3d817def24fb0abb73
Gabe Macri <gabemacru@gmail.com>
2023-12-06 12:12:23 -0500

    jsupdate

---
+++ scripts/index.3.js
@@ -0,0 +1,9 @@
+// Initialization for ES Users
+import { Ripple, initMDB } from "mdb-ui-kit";
+
+initMDB({ Ripple });
+
+// Initialization for ES Users
+import { Carousel, initMDB } from "mdb-ui-kit";
+
+initMDB({ Carousel });
